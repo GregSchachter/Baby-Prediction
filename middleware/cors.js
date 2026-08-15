b30f7027032bc778c1242a5e1ad02d6c8e46af88
GregSchachter <gregschachter@gmail.com>
2026-08-15 14:30:05 -0400

    Update cors.js

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,6 +2,8 @@ const cors = require("cors");
 require("dotenv").config();
 const frontEndlink = process.env.FRONTEND_URL;
 
+console.log("frontend", frontEndlink);
+
 var corsOptions = {
   origin: [frontEndlink],
   allowedHeaders: ["Content-Type", "Authorization"],
