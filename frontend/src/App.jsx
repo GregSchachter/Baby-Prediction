a706f8d53ab1990c32bfd7d4da2e96170fa0ad99
GregSchachter <gregschachter@gmail.com>
2026-08-15 13:59:22 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,6 +35,8 @@ function App() {
           user: res.data.user || null,
           hasPredicted: res.data.hasPredicted || false,
         });
+
+        alert(JSON.stringify res.data)
       } catch (error) {
         console.log("Server health check failed:", error.message);
 
